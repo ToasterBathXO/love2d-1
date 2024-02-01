dc0b68f073c2fa5cede65e36b0de95f77fa92e6f
fortunaadivit <100645982+PatrickMQU@users.noreply.github.com>
2024-02-02 08:48:41 +0900

    Config

---
+++ main.lua
@@ -0,0 +1,12 @@
+
+-- Background
+
+function love.draw()
+    -- Set Background Color to #3399FF
+    red = 51/255
+    green = 153/255
+    blue = 255/255
+    alpha = 50/100
+    love.graphics.setBackgroundColor( red, green, blue, alpha)
+end
+
