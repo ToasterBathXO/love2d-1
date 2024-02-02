b1e2e1549c3df7971af1869e7def35ae911b3702
fortunaadivit <100645982+PatrickMQU@users.noreply.github.com>
2024-02-02 09:01:29 +0900

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,8 +1,11 @@
+-- Config setup source - https://www.youtube.com/watch?v=odwEphAwX5M&list=PLYBJzqz8zpWYip5ZkTMQiOkqya9Iiefm9&index=1
 function love.conf(t)
     t.identity="data/saves"
     t.version="1.0.0"
     t.console = false
     t.externalstorage =true
     t.gammacorrect = true
-
+    t.window.title = "Love2dTestGame"
+    t.window.width = 1200
+    t.window.height = 700
 end
