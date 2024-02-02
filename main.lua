c2010cf381564fdd6afcf810c5d1e95a7658bed9
fortunaadivit <100645982+PatrickMQU@users.noreply.github.com>
2024-02-02 09:17:26 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,20 +10,6 @@ end
 
 
 
-
-
-
--- Background
-function love.draw()
-    -- Set Background Color to #3399FF
-    red = 51/255
-    green = 153/255
-    blue = 255/255
-    alpha = 50/100
-    love.graphics.setBackgroundColor( red, green, blue, alpha)
-end
-
-
 -- Ground
 function love.draw()
     love.graphics.rectangle("fill", 0, 300, 1200, 100)
